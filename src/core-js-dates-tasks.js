8501d5fe4a10d16ca254bd477cf30b546d0751e9
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-01-03 16:55:43 +0300

    feat: add solution getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -210,8 +210,17 @@ function getWeekNumberByDate(/* date */) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  let i = date.getMonth();
+  date.setDate(13);
+  while (i < 12) {
+    date.setMonth(i);
+    if (date.getUTCDay() === 4) {
+      return date;
+    }
+    i += 1;
+  }
+  return date;
 }
 
 /**
